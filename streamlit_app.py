9a318e16086064c816a3eff24f01c5d3a8ed56ce
TarandeepSingh6267 <164173630+TarandeepSingh6267@users.noreply.github.com>
2024-08-13 14:46:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,10 @@ import streamlit as st
 from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 import pandas as pd
-
+# importing necessary libraries
+import img2pdf
+from PIL import Image
+import os
 # Write directly to the app
 st.title("Zena's Amazing Athleisure Catalog")
 st.write(
@@ -14,15 +17,38 @@ st.write(
 # Get the current credentials
 uploaded_file = st.file_uploader("Choose an image file", accept_multiple_files=False, label_visibility='hidden')
 if uploaded_file is not None:
-  # Convert image base64 string into hex 
-  bytes_data_in_hex = uploaded_file.getvalue().hex()
-
-  # Generate new image file name
-  file_name = 'img_' + str(uuid.uuid4())
-
-  # Write image data in Snowflake table
-  df = pd.DataFrame({"FILE_NAME": [file_name], "IMAGE_BYTES": [bytes_data_in_hex]})
-  session.write_pandas(df, "IMAGES")
+    
+        # Python3 program to convert image to pdf
+    # using img2pdf library
+     
+    
+     
+    # storing image path
+    img_path = "C:/Users/Admin/Desktop/GfG_images/do_nawab.png"
+     
+    # storing pdf path
+    pdf_path = "C:/Users/Admin/Desktop/GfG_images/file.pdf"
+     
+    # opening image
+    image = Image.open(img_path)
+     
+    # converting into chunks using img2pdf
+    pdf_bytes = img2pdf.convert(image.filename)
+     
+    # opening or creating pdf file
+    file = open(pdf_path, "wb")
+     
+    # writing pdf files with chunks
+    file.write(pdf_bytes)
+     
+    # closing image file
+    image.close()
+     
+    # closing pdf file
+    file.close()
+     
+    # output
+    print("Successfully made pdf file")
 '''
 my_catlog=session.table('catalog_for_website').select(col('COLOR_OR_STYLE'))
 
